e178c9aec24781e4efffb3a37660a255c63384ea
VuVT <truongvuuit@gmail.com>
2021-11-13 11:05:50 +0700

    Review

--- Ex_2021_11_08/Array_min.rb
+++ Ex_2021_11_08/Array_min.rb
@@ -1,12 +1,12 @@
-arr_n = Array.new 
+arr_n = Array.new # biến chứa nhiều phần tử nên là số nhiều
 puts "nhap -1 de ngung "
 while arr_n.length < 100 do
     begin 
         puts "nhap so phan tu: "  
-        n = Integer(gets)
-        if n == -1 then break          
+        n = Integer(gets) # tên biến cần đặt có ý nghĩa hơn
+        if n == -1 then break # không cần then, nên xuống dòng cho dễ nhin         
         elsif n >= 0 then arr_n.push(n)         
-        else puts "nhap so phan tu: "    
+        else puts "nhap so phan tu: " #nên xuống dòng cho dễ nhin
             
         end
     end 
@@ -16,3 +16,13 @@ arr_numbers = arr_n.sort
 min = arr_numbers[0]
 puts "mang n: #{arr_numbers}"
 puts "so nho nhat trong mang la #{min}"
+
+# Chương trình e viết rất ngắn gọn,  
+# Một lỗi nghiêm trọng là khi user input không đúng thì chương trình của e thoát ra luôn, và không yêu cầu nhập lại
+# Vd: 
+# nhap -1 de ngung 
+# nhap so phan tu: 
+# abcabc
+# Traceback (most recent call last):
+#     1: from main.rb:6:in `<main>'
+# main.rb:6:in `Integer': invalid value for Integer(): "abcabc\n" (ArgumentError)

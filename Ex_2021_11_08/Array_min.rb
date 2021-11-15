28acc8ab3155b06cce11c57e26033729e55d2271
NhatTan vn <53886414+NhatTanvn@users.noreply.github.com>
2021-11-15 15:13:07 +0700

    [Exercise 3] change variable

--- Ex_2021_11_08/Array_min.rb
+++ Ex_2021_11_08/Array_min.rb
@@ -1,22 +1,23 @@
 arr_numbers = Array.new 
+STOP_SIGNAL = -1
 puts "nhap -1 de ngung "
 while arr_numbers.length < 100 do
     begin 
-        puts "nhap so phan tu: "  
-        number = Integer(gets)
+        puts "nhap so phan tu: "      
+         number = Integer(gets)       
     rescue     
         puts "Vui long nhap so!" 
         retry
     end 
-    if number == -1 
+    if number == STOP_SIGNAL 
          break          
     elsif number >= 0
          arr_numbers.push(number)         
     else 
-        puts "nhap so phan tu: "          
+        puts "Vui long nhap so lon hon 0!"          
     end
 
 end
 array_min = arr_numbers.min
-puts "mang n: #{arr_numbers}"
+puts "Day so nguyen la: #{arr_numbers}"
 puts "so nho nhat trong mang la #{array_min}"
